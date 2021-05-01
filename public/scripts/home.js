c854396edb976dd46996789d589a8bff62bc0fc5
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-01 12:55:21 +0530

    make it better

---
+++ public/scripts/home.js
@@ -0,0 +1,26 @@
+var lastScrollTop = 0;
+var navbar = document.getElementById("nav-area")
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if(scrollTop > lastScrollTop){
+        navbar.style.top = "-200px";
+    }
+    else{
+        navbar.style.top = "0";
+    }
+
+    lastScrollTop = scrollTop;
+})
+
+// Auto Image scroll----------------------------------------------------
+let counter = 1;
+setInterval(function(){
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+},8000);
+
+console.log("HelLo chetan")
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 80;
+
+
+//Express Setup ---------------------------------------------------------------------------------------------
+const app = express();
+app.use('/public', express.static('public'));
+
+//PUG Setup--------------------------------------------------------------------------------------------------
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//ENDPOINTS-------------------------------------------------------------------------------------------------
+app.get('/', (req, res) => {
+    res.status(200).render('home.pug');
+})
+
+
+
+
+//Listen ---------------------------------------------------------------------------------------------------
+app.listen(port, () =>{
+    console.log(`The app is running on the port : ${port}`);
+})
