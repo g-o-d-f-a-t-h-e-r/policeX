344cfc4f4ac5a51680a46ecbcc665d1361573916
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-03 09:21:25 +0530

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://policex.herokuapp.com','https://policex.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://policex.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
