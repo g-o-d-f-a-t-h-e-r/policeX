e777438c132c1c8768c1e041f2a8920af7c2141a
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-03 18:13:40 +0530

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const { createHash } = require('crypto');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 
 const port = process.env.PORT || 80;
 
 //Express Setup ---------------------------------------------------------------------------------------------
 const app = express();
+
+app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  });
+
+
 app.use('/public', express.static('public'));
 
 //PUG Setup--------------------------------------------------------------------------------------------------
