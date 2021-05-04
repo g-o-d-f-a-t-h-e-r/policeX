8959f83af62444f167c238f05932c364430a3027
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-04 09:14:18 +0530

    make it better

--- server.js
+++ server.js
@@ -9,10 +9,23 @@ const port = process.env.PORT || 80;
 //Express Setup ---------------------------------------------------------------------------------------------
 const app = express();
 
-app.use(cors());
+app.use(function (req, res, next) {
 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 
