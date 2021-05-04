d1ed82f174e9e5a4dca87ea63408189b90f11724
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-04 08:54:39 +0530

    make it better

--- public/scripts/news.js
+++ public/scripts/news.js
@@ -5,7 +5,7 @@ let news2 = document.querySelector('.news2');
 
 
 fetch('http://newsapi.org/v2/top-headlines?country=in&apiKey=e56e87add54441be86d6d05eb30b5134', {
-        mode: 'no-cors'
+        
 })
     .then(res => {
         return res.json()
