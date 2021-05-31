a321e931c4c3d8b1cc387b3ee2379abfccf9a75e
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2021-05-31 17:51:25 +0530

    bold e-mail

--- server.js
+++ server.js
@@ -238,7 +238,7 @@ app.post('/forgotPassword', async(req, res) => {
                 to : emailAdd,
                 subject : `Police X Forgot Password`,
                 html : `<h3>Hello ${emailAdd}</h3>
-                        <p> Your OTP for resetting the Password is : <h3>${random[emailAdd]}</h3>`
+                        <p> Your OTP for resetting the Password is : <h3 style='display : inline;'>${random[emailAdd]}</h3>`
 
             }
 
