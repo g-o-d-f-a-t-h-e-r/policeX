7e90173e59145b1fab5bbcf65270aedc4fdff026
g-o-d-f-a-t-h-e-r <67109203+g-o-d-f-a-t-h-e-r@users.noreply.github.com>
2021-06-14 14:08:12 +0530

    Delete creds directory

--- creds/url.js
+++
@@ -1,7 +0,0 @@
-const url = {
-    url : `mongodb+srv://chetan__008:chetan@1234@cluster0.nkxr5.mongodb.net/Police-X?retryWrites=true&w=majority`,
-    JWT_SECRET : `shfdjsfhuer89489fije490ur9@@*@(*FHN#*RNF(#*#&RFN#HIHIUHFUIHdkhfskjdhfe48y843984*&*^&HIHKJ`,
-    secret : `mgfjdhgd4985r7fmw@87483m5kjhdfdhjw$@UM*@(*$#M*F*V`
-}
-
-module.exports = url;
